a2b55b5205c2ef880ca3e480664c2152e8c63dca
aikesi10 <1828355613@qq.com>
2026-05-08 16:27:32 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import PetDiaryApp from './pet_diary.jsx';
+import PetDiaryApp from './PetDiaryApp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
